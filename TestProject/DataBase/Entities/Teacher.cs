e18130b1fd79bd3fa8782510250ff3e797a76700
Spay <spay1991@i.ua>
2018-09-20 16:15:28 +0300

    12

--- TestProject/DataBase/Entities/Teacher.cs
+++ TestProject/DataBase/Entities/Teacher.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TestProject.DataBase.Entities
 {
     [Table("Teacher")]
     public class Teacher
     {
+        [Column("id")]
         public int Id { get; set; }
 
         [Column("fullname")]
@@ -12,5 +14,7 @@ namespace TestProject.DataBase.Entities
 
         [Column("phone")]
         public string Phone { get; set; }
+
+        public virtual ICollection<Discipline> Disciplines { get; set; }
     }
 }
--- TestProject/DataBase/Entities/TeacherDisciple.cs
+++ TestProject/DataBase/Entities/TeacherDisciple.cs
@@ -1,14 +1,17 @@
-﻿//using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
-//namespace TestProject.DataBase.Entities
-//{
-//    [Table("TEACHER_DISCIPLINE")]
-//    public class TeacherDisciple
-//    {
-//        [Column("id_teacher")]
-//        public int TeacherId { get; set; }
+namespace TestProject.DataBase.Entities
+{
+    [Table("TEACHER_DISCIPLINE")]
+    public class TeacherDisciple
+    {
+        [Column("id")]
+        public int Id { get; set; }
 
-//        [Column("id_discipline")]
-//        public int DisciplineId { get; set; }
-//    }
-//}
+        [Column("id_teacher")]
+        public int TeacherId { get; set; }
+
+        [Column("id_discipline")]
+        public int DisciplineId { get; set; }
+    }
+}
